f9fa9856983f620550a548ea17987067ebffbd1d
Benjamin Gruenbaum <Benjamin@Benjamins-MacBook-Pro.local>
2015-08-03 09:11:39 +0300

    life is too short for caps

--- views/login.js
+++ views/login.js
@@ -14,7 +14,12 @@
 
         if(login(form.user.value, form.passwd.value)){
             var view = document.getElementsByClassName("view")[0];
-            view.innerHTML = "<h4>Welcome " + sessionStorage.getItem("username") +" </h4>";
+            view.innerHTML = "<h4>Welcome " + sessionStorage.getItem("username") +". You are logged in successfully.</h4>";
+            view.innerHTML += "<button id='logoutButton'> Logout </button>";
+            document.getElementById("logoutButton").onclick = function(){
+                sessionStorage.removeItem("username");
+                location.reload();
+            };
         } else {
             alert("Failed, now u get rolled >:( Passwords are four digits.");
             location.href = "http://rebecca.blackfriday";
